a8e38f17c507328dc88eb391c23090018d966ea2
poorna99122 <poornachandrarao99122@gmail.com>
2021-09-16 19:23:15 -0700

    added table and given dummy data to that table in employee list component

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+
 
 @Component({
   selector: 'app-employee-list',
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmployeeListComponent implements OnInit {
 
+
+  // This property we can use in employee list html
+  employees : Employee[];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.employees = [{
+      "id" :1,
+      "firstName" : "Ramesh",
+      "lastName" : "suresh",
+      "emailId" : "Ramesh@gmail",
+
+    },{
+      "id" :1,
+      "firstName" : "poorna",
+      "lastName" : "chandra",
+      "emailId" : "poorna@gmail",
+    }];
+
   }
 
 }
