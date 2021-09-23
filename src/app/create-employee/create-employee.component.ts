4298fbe91646ee154bd5811e6e82c49720b7857f
poorna99122 <poornachandrarao99122@gmail.com>
2021-09-22 22:49:40 -0700

    Added create employee POST API

--- src/app/create-employee/create-employee.component.ts
+++ src/app/create-employee/create-employee.component.ts
@@ -1,5 +1,7 @@
+import { EmployeeService } from './../employee.service';
 import { Employee } from './../employee';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-employee',
@@ -10,14 +12,28 @@ export class CreateEmployeeComponent implements OnInit {
 
 
   employee : Employee = new Employee();
-  constructor() { }
+  constructor(private employeeService : EmployeeService,
+    private router : Router
+    ) { }
 
   ngOnInit(): void {
+  }
+  saveEmployee(){
+    this.employeeService.createEmployee(this.employee).subscribe( data =>{
+      console.log(data);
+      this.goToEmployeeList();
+    },
+    error => console.log(error));
+  }
+
+  goToEmployeeList(){
+    this.router.navigate(['/employees']);
   }
 
 
   onSubmit(){
-    
+    console.log(this.employee);
+    this.saveEmployee();
   }
 
 }
